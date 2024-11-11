b3fec299acbc1069811eefe0f3773f926647169a
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2024-11-11 19:22:29 -0300

    Anexação de arquivos de projeto Spring Boot com autenticação JWT

--- boot-crud/src/main/java/com/javainuse/boot_crud/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javainuse.boot_crud.repository;
-
-import com.javainuse.boot_crud.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
